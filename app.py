731421042992e2ae3eb82241240dcfc72893cb72
tuflesa <tecnico@tuflesa.es>
2021-05-24 12:46:56 +0200

    modificado timeout

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ CORS(app)
 
 def leer_bascula():
     try:
-        bascula = serial.Serial('/dev/ttyUSB0', timeout=1)
+        bascula = serial.Serial('/dev/ttyUSB0', timeout=2)
         peso = bascula.readline()
         bascula.close()
         peso = peso.decode("utf-8")
